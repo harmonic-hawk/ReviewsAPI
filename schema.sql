0d4b066d2775a97ed87510c04ae0f6349022bf00
juan mejia <juanvmejia01@gmail.com>
2021-09-29 14:18:45 -0700

    added a characteristics_reviews table

--- schema.sql
+++ schema.sql
@@ -4,12 +4,12 @@ CREATE TABLE reviews(
   Id INT NOT NULL AUTO_INCREMENT,
   product_id INT NOT NULL,
   rating INT NOT NULL,
-  helpfulness INT NOT NULL,
+  helpfulness INT NOT NULL DEFAULT 0,
   date DATE NOT NULL,
   recommend BOOLEAN NOT NULL,
   reported BOOLEAN NOT NULL,
-  name VARCHAR NOT NULL,
-  email VARCHAR NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL,
   summary TEXT NOT NULL,
   body TEXT NOT NULL,
   response TEXT,
@@ -19,23 +19,21 @@ CREATE TABLE reviews(
 CREATE TABLE images(
   Id INT NOT NULL AUTO_INCREMENT,
   review_id INT NOT NULL,
-  url VARCHAR(600) NOT NULL,
+  url text NOT NULL,
   FOREIGN KEY (review_id) REFERENCES reviews(id)
 );
-  
-CREATE TABLE meta(
-  id INT NOT NULL AUTO_INCREMENT,
-  product_id INT NOT NULL,
-  recommended INT NOT NULL,
-  FOREIGN KEY (product_id) REFERENCES reviews(product_id)
-);
-
 
 CREATE TABLE characteristics(
   id INT NOT NULL,
   product_id INT NOT NULL,
-  Size INT NOT NULL,
-  Width INT NOT NULL,
-  Comfort INT NOT NULL,
-  FOREIGN KEY (product_id) REFERENCES reviews(product_id)
+  name: VARCHAR(50)
 );
+
+CREATE TABLE characteristics_reviews(
+  id INT NOT NULL AUTO_INCREMENT,
+  review_id INT NOT NULL,
+  characteristics_id INT NOT NULL,
+  value: INT,
+  FOREIGN KEY (review_id) REFERENCES reviews(id)
+  FOREIGN KEY (characteristics_id) REFERENCES characteristics(id)
+  )
