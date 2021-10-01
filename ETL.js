44dbfa12f2cc629d76a065df964407b73d6fb089
juan mejia <juanvmejia01@gmail.com>
2021-10-01 03:15:50 -0700

    Added ETL code and adapted the database to match the CSV file

---
+++ ETL.js
@@ -0,0 +1,23 @@
+LOAD DATA LOCAL INFILE '/Users/juanvmeji/Desktop/SDC/reviews.csv'
+ INTO TABLE reviews
+ FIELDS TERMINATED BY ','
+ ENCLOSED BY '"'
+ LINES TERMINATED BY '\n'
+ IGNORE 1 ROWS
+ ;
+
+ LOAD DATA LOCAL INFILE '/Users/juanvmeji/Desktop/SDC/reviews_photos.csv'
+ INTO TABLE images
+ FIELDS TERMINATED BY ','
+ ENCLOSED BY '"'
+ LINES TERMINATED BY '\n'
+ IGNORE 1 ROWS
+ ;
+
+ LOAD DATA LOCAL INFILE '/Users/juanvmeji/Desktop/SDC/characteristic_reviews.csv'
+ INTO TABLE characteristics
+ FIELDS TERMINATED BY ','
+ ENCLOSED BY '"'
+ LINES TERMINATED BY '\n'
+ IGNORE 1 ROWS
+ ;
--- schema.sql
+++ schema.sql
@@ -1,39 +1,65 @@
-CREATE DATABASE ratings_reviews
+DROP DATABASE IF EXISTS ratings_reviews;
 
-CREATE TABLE reviews(
-  Id INT NOT NULL AUTO_INCREMENT,
+CREATE DATABASE ratings_reviews;
+
+use ratings_reviews;
+
+-- id,product_id,rating,date,summary,body,recommend,reported,reviewer_name,reviewer_email,response,helpfulness
+-- 1,1,5,1596080481467,“This product was great!“,”I really did or did not like this product based on whether it was sustainably sourced.  Then I found out that its made from nothing at all.“,true,false,“funtime”,“first.last@gmail.com”,null,8
+
+CREATE TABLE reviews (
+  id INT NOT NULL AUTO_INCREMENT,
   product_id INT NOT NULL,
   rating INT NOT NULL,
-  helpfulness INT NOT NULL DEFAULT 0,
   date DATE NOT NULL,
-  recommend BOOLEAN NOT NULL,
-  reported BOOLEAN NOT NULL,
-  name VARCHAR(50) NOT NULL,
-  email VARCHAR(50) NOT NULL,
   summary TEXT NOT NULL,
   body TEXT NOT NULL,
+  recommend BOOLEAN NOT NULL,
+  reported BOOLEAN NOT NULL,
+  reviewer_name VARCHAR(50) NOT NULL,
+  reviewer_email VARCHAR(50) NOT NULL,
   response TEXT,
-  PRIMARY KEY(Id)
+  helpfulness INT NOT NULL DEFAULT 0,
+  PRIMARY KEY (id)
 );
 
+-- id,review_id,url
+-- 1,5,“https://images.unsplash.com/photo-1560570803-7474c0f9af99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80”
+-- 2,5,“https://images.unsplash.com/photo-1561693532-9ff59442a7db?ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80”
+-- 3,5,“https://images.unsplash.com/photo-1487349384428-12b47aca925e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80”
+
 CREATE TABLE images(
-  Id INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   review_id INT NOT NULL,
   url text NOT NULL,
-  FOREIGN KEY (review_id) REFERENCES reviews(id)
+  FOREIGN KEY (review_id) REFERENCES reviews(id),
+  PRIMARY KEY(id)
 );
+
+-- id,product_id,name
+-- 1,1,“Fit”
+-- 2,1,“Length”
+-- 3,1,“Comfort”
+-- 4,1,“Quality”
 
 CREATE TABLE characteristics(
   id INT NOT NULL,
   product_id INT NOT NULL,
-  name: VARCHAR(50)
+  name VARCHAR(50),
+  PRIMARY KEY(id)
 );
+
+-- id,characteristic_id,review_id,value
+-- 1,1,1,4
+-- 2,2,1,3
+-- 3,3,1,5
 
 CREATE TABLE characteristics_reviews(
   id INT NOT NULL AUTO_INCREMENT,
-  review_id INT NOT NULL,
   characteristics_id INT NOT NULL,
-  value: INT,
-  FOREIGN KEY (review_id) REFERENCES reviews(id)
-  FOREIGN KEY (characteristics_id) REFERENCES characteristics(id)
+  review_id INT NOT NULL,
+  value INT,
+  FOREIGN KEY (review_id) REFERENCES reviews(id),
+  FOREIGN KEY (characteristics_id) REFERENCES characteristics(id),
+  PRIMARY KEY(id)
   )
